98734bb570b21552c4c119a206a1727962cfa7e4
Valentin Kolb <valentinolb@uni-ulm.de>
2021-06-10 17:35:05 +0200

    updated install script

--- install.sh
+++ install.sh
@@ -1,28 +1,17 @@
 #!/usr/bin/env bash
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+echo "Installing pyginx ..."
 
-  echo "Installing pyginx ..."
+echo "Installing pip3 ..."
+sudo apt update
+sudo apt install python3-pip -y
 
-  echo "Installing pip3 ..."
-  sudo apt update
-  sudo apt install python3-pip -y
+echo "Installing requirements.txt ..."
 
-  echo "Installing requirements.txt ..."
+curl https://raw.githubusercontent.com/ValentinKolb/pyginx/main/requirements.txt | pip3 install -r
 
-  curl https://raw.githubusercontent.com/ValentinKolb/pyginx/main/requirements.txt | pip3 install -r
+curl https://raw.githubusercontent.com/ValentinKolb/pyginx/main/pyginx > /usr/local/bin/pyginx
+sudo chmod +x /usr/local/bin/pyginx
 
-  curl https://raw.githubusercontent.com/ValentinKolb/pyginx/main/pyginx > /usr/local/bin/pyginx
-  sudo chmod +x /usr/local/bin/pyginx
-
-  echo "... complete ! run 'sudo pyginx -h' to get started"
-  exit 0
-
-else
-
-  echo "Your OS is not supported!"
-  exit 1
-
-fi
-
-
+echo "... complete ! run 'sudo pyginx -h' to get started"
+exit 0
